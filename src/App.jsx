8bc3b7dfb9602a128952dfcc7fdc645b92a0e661
gauravkharel <kharel619@gmail.com>
2022-12-04 16:18:55 +0545

    component composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useState, StrictMode } from "react";
 import SearchParams from "./SearchParams";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h1>Pets for soul</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <div className="App">
+        <h1>Pets for soul</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 }
 
--- src/useBreedList.jsx
+++ src/useBreedList.jsx
@@ -3,30 +3,30 @@ import { useState, useEffect } from "react";
 const localCache = {};
 
 export default function useBreedList(animal) {
-    const [breedList, setBreedList] = useState([]);
-    const [status, setStatus] = useState("unloaded")
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
 
-    useEffect(() => {
-        if (!animal) {
-            setBreedList([])
-        } else if (localCache[animal]) {
-            setBreedList(localCache[animal])
-        } else {
-            requestBreedList()
-        }
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
 
-        async function requestBreedList() {
-            setBreedList([]);
-            setStatus("loading");
-            const res = await fetch(
-                `http://pet-v2.dev-apis.com/breeds?animal=${animal}`
-            )
-            const json = await res.json();
-            localCache[animal] = json.breeds || [];
-            setBreedList(localCache[animal]);
-            setStatus("loaded");
-        }
-    }, [animal]);
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
 
-    return [breedList, status];
+  return [breedList, status];
 }
