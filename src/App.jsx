0a6783a153696c1a95413fc4719bb0fb349de979
gauravkharel <kharel619@gmail.com>
2022-12-04 13:58:34 +0545

    useEffect implementation

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Pets for soul</h1>
       <SearchParams />
     </div>
   );
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,67 +1,89 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog"];
 
 const SearchParams = () => {
-  const [location, updateLocation] = useState("");
-  const [animal, updateAnimal] = useState(ANIMALS[2]);
-  const [breed, updateBreed] = useState("");
-  const breeds = [];
-  return (
-    <div className="search-params">
-      <form>
-        <label htmlFor="location">
-          Location
-          <input
-            id="location"
-            value={location}
-            placeholder="Location"
-            onChange={(e) => updateLocation(e.target.value)}
-          />
-        </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => {
-              updateAnimal(e.target.value);
-              updateBreed("");
-            }}
-            onBlur={(e) => {
-              updateAnimal(e.target.value);
-              updateBreed("");
-            }}
-          >
-            <option />
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            disabled={!breeds.length}
-            id="breed"
-            value={breed}
-            onChange={(e) => updateBreed(e.target.value)}
-            onBlur={(e) => updateBreed(e.target.value)}
-          >
-            <option />
-            {breeds.map((breed) => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+    const [pets, setPets] = useState([])
+    const [location, updateLocation] = useState("");
+    const [animal, updateAnimal] = useState("");
+    const [breed, updateBreed] = useState("");
+    
+    const breeds = [];
+    
+    useEffect(() => {
+        requestPets();
+    }, [])
+    //the [] at the end of the useEffect is where you declare your data dependencies. React wants to know when to run that effect again. You don't give it data dependencies, it assumes any time any hook changes that you should run the effect again. This is bad because that would mean any time setPets gets called it'd re-run render and all the hooks again. See a problem there? It'd run infinitely since requestPets calls setPets.
+
+    async function requestPets() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+        )
+        const json = await res.json()
+        setPets(json.pets)
+    }
+
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    Location
+                    <input
+                        id="location"
+                        value={location}
+                        placeholder="Location"
+                        onChange={(e) => updateLocation(e.target.value)}
+                    />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={(e) => {
+                            updateAnimal(e.target.value);
+                            updateBreed("");
+                        }}
+                        onBlur={(e) => {
+                            updateAnimal(e.target.value);
+                            updateBreed("");
+                        }}
+                    >
+                        <option />
+                        {ANIMALS.map((animal) => (
+                            <option key={animal} value={animal}>
+                                {animal}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        disabled={!breeds.length}
+                        id="breed"
+                        value={breed}
+                        onChange={(e) => updateBreed(e.target.value)}
+                        onBlur={(e) => updateBreed(e.target.value)}
+                    >
+                        <option />
+                        {breeds.map((breed) => (
+                            <option key={breed} value={breed}>
+                                {breed}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+                <button>Submit</button>
+            </form>
+            {
+                pets.map((pet) => (
+                    <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+                ))
+            }
+        </div>
+    );
 };
 
 export default SearchParams;
