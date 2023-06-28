7a27a3493f55110c7892f01e470f58602991a274
gauravkharel <kharel619@gmail.com>
2023-06-28 18:27:47 +0545

    implementing react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState, StrictMode } from "react";
 import SearchParams from "./SearchParams";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    }
+  }
+})
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,11 +20,14 @@ function App() {
     <StrictMode>
       <div className="App">
         <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
           <Link to="/"><h1>Adopt Me!</h1></Link>
           <Routes>
             <Route path="/details/:id" element={<Details />} />
             <Route path="/" element={<SearchParams />} />
           </Routes>
+          </QueryClientProvider>
+          
         </BrowserRouter>
       </div>
 
