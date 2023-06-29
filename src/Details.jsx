122c5293b16a8592a11e3ba64154fcc68c058068
gauravkharel <kharel619@gmail.com>
2023-06-29 09:22:35 +0545

    implementing error boundaries for pet details

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,14 @@
 import { useParams } from "react-router-dom";
 import {useQuery} from "@tanstack/react-query"
 import fetchPet from "./fetchPet";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary"
 
-const Details = () => {
+export const Details = () => {
     const {id} = useParams()
     const results = useQuery(["details", id], fetchPet)
+
+    
 
     if(results.isLoading) {
         return (
@@ -17,6 +21,7 @@ const Details = () => {
         const pet  = results.data.pets[0]
         return (
             <div className="details">
+                <Carousel images={pet.images}  />
                 <div>
                     <h1>{pet.name}</h1>
                     <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
@@ -28,4 +33,11 @@ const Details = () => {
     )
     
 }
-export default Details;
+
+export default function DetailsErrorBoundary(props){
+    return(
+        <ErrorBoundary>
+            <Details {...props} />
+        </ErrorBoundary>
+    )
+}
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -18,10 +18,6 @@ const SearchParams = () => {
     const results = useQuery(["search", requestParams], fetchSearch)
     const pets = results?.data?.pets ?? [];
 
-   
-
-
-
     return (
         <div className="search-params">
             <form onSubmit={e => 
